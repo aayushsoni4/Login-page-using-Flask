cbb182142d5c7f8a12c246d42892de4670120b59
Aayush <aayushsoni424@gmail.com>
2023-08-31 23:43:13 +0530

    Created Frontend

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def login():
     return render_template('login.html')
 
 @app.route('/register')
-def about():
+def register():
     return render_template('register.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
+@app.route('/login_validation', methods=['POST', 'GET'])
+def login_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    return f'Email: {email} Password: {password}'    
 
 if __name__=='__main__':
     app.run(debug=True)
